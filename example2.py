3f04d48bef08f70abc442011753b013e1aff387b
moazzampasha <157400754+moazzampasha@users.noreply.github.com>
2024-01-29 21:22:56 +0500

    Add files via upload

---
+++ example2.py
@@ -0,0 +1,15 @@
+#input number
+num=int(input("enter num: "))
+if num%2 ==0:  print("even number")
+else: print("odd num")
+
+
+
+
+#largest number
+a=int(input("enter a: "));
+b=int(input("enter b: "));
+c=int(input("enter c: "));
+if a>b and a>c:    print("a is largest")
+if b>a and b>c:    print ("b is largest")
+if c>a and c>b:    print("c is largest")
