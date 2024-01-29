ade5d4787b5f30f429e35ed2e45d28989dbb7f4f
moazzampasha <157400754+moazzampasha@users.noreply.github.com>
2024-01-29 21:53:53 +0500

    Add files via upload

---
+++ example3.py
@@ -0,0 +1,62 @@
+#Check Condition for vote
+'''age=int(input("enter age: "))
+if age>=18: print("you r eligible to vote")
+else:
+    print("sorry you r not eligible to vote")'''
+
+
+#Elif example
+'''number=int(input("enter num: "))
+if number==10: print("given num is 10")
+elif number==50: print("given num is 50") 
+else: print("given num is not equal to 10,50")''' 
+
+# FOR loop
+'''languagues= ['swift','python','java']
+for languague in languagues:   print(languague)'''
+
+
+#WHILE Loop
+'''number=0
+while number < 10: print(f"bano qabil{number}")
+number=number+1'''
+
+
+#Pre Define Functions
+'''import math
+x=math.sqrt(40)
+print(x)'''
+
+'''import math
+x=math.ceil(1.4)
+y=math.floor(1.4)
+print(x)'''
+
+
+#USER DEFINED FUNCTION
+'''def bq():  print("bano qabil")
+bq()'''
+
+
+'''def add(a,b): return a+b  
+defistrue(a):return bool(a)
+res=add(2,3); print("result of add function is {}":format(res))
+res =true{2<5}
+print("/n result is true{}".format(res))'''
+
+#DICTIONARY
+'''ages={"moazzam":21,"pasha":20}
+for student in ages: print(student)'''
+
+'''ages={14,101,10}
+print(ages)'''
+
+'''languagues=("python","swift","c++")
+print(languagues[1])'''
+
+#list slicing in python 
+mylist=['p','q','r','w']
+print(mylist[3])
+
+    
+
